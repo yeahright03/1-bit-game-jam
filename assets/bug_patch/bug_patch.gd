abad621872725b13e6286462110aff302e53fd14
yeahright03 <martinjansander@gmail.com>
2026-08-05 17:25:06 +0200

    added bug_patch

---
+++ assets/bug_patch/bug_patch.gd
@@ -0,0 +1,44 @@
+extends Node3D
+
+signal swatted
+signal died
+
+@export var debug_text : bool = false
+
+@export var max_health : int = 5
+@export var min_health : int = 2
+var health : int
+
+
+@export var max_size : float = 1.5
+@export var min_size : float = .75
+var size : float
+var size_decrease_rate : float
+
+@onready var scaling : Node3D = $scaling
+
+func _ready() -> void:
+    health = randi_range(max_health, min_health)
+    size = randf_range(max_size, min_size)
+    self.scale *= size
+    size_decrease_rate = size / health
+    if debug_text:
+        print('%s\nHealth: %s\nSize: %s\nSize decrease rate: %s' % [self.name, health, size, size_decrease_rate])
+
+func _unhandled_key_input(_event: InputEvent) -> void:
+    if Input.is_key_pressed(KEY_ENTER):
+        self.swat()
+
+
+func swat():
+    health -= 1
+    var decrease_size : Vector3 = Vector3(size_decrease_rate, size_decrease_rate, size_decrease_rate)
+    scaling.scale -= decrease_size
+    swatted.emit()
+    if debug_text:
+        print('%s took damage!' % self.name)
+    if health == 0:
+        queue_free()
+        died.emit()
+        if debug_text:
+            print('%s died!' % self.name)
