9ec5e72fd5ba26eb021a6b2b99c66a5f258ea0ad
yeahright03 <martinjansander@gmail.com>
2026-08-06 12:19:41 +0200

    comments on bug_patch.gd and renamed scaling node name
    also gave debug text a way of telling the current health after being hit

--- assets/bug_patch/bug_patch.gd
+++ assets/bug_patch/bug_patch.gd
@@ -3,20 +3,22 @@ extends Node3D
 signal swatted
 signal died
 
+# enable to view the debug text
 @export var debug_text : bool = false
 
 @export var max_health : int = 5
 @export var min_health : int = 2
 var health : int
 
-
 @export var max_size : float = 1.5
 @export var min_size : float = .75
 var size : float
 var size_decrease_rate : float
 
-@onready var scaling : Node3D = $scaling
+@onready var scaling_node : Node3D = $scaling_node
 
+
+# establishes the values for the bug patch
 func _ready() -> void:
 	health = randi_range(max_health, min_health)
 	size = randf_range(max_size, min_size)
@@ -25,18 +27,19 @@ func _ready() -> void:
 	if debug_text:
 		print('%s\nHealth: %s\nSize: %s\nSize decrease rate: %s' % [self.name, health, size, size_decrease_rate])
 
+# only for debug usage to test damage systems
 func _unhandled_key_input(_event: InputEvent) -> void:
 	if Input.is_key_pressed(KEY_ENTER):
 		self.swat()
 
-
+# the function is used to damage the bug patch and to decrease its size whenever it takes damage
 func swat():
 	health -= 1
 	var decrease_size : Vector3 = Vector3(size_decrease_rate, size_decrease_rate, size_decrease_rate)
-	scaling.scale -= decrease_size
+	scaling_node.scale -= decrease_size
 	swatted.emit()
 	if debug_text:
-		print('%s took damage!' % self.name)
+		print('%s took damage! Current health: %s' % [self.name, health])
 	if health == 0:
 		queue_free()
 		died.emit()
